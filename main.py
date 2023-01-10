7c62fbfa3afc0596db4b6e9d4bd038401d3d3a6f
imahdimir <imahdimir@gmail.com>
2023-01-10 21:10:47 +0330

    updated

--- main.py
+++ main.py
@@ -1,86 +1,97 @@
 """
 
-  """
+    """
 
-from githubdata import GithubData
-from mirutil.df_utils import save_df_as_a_nice_xl as snxl
+from pathlib import Path
 
+import pandas as pd
+from githubdata import GitHubDataRepo
+from mirutil.df import save_df_as_a_nice_xl
+from mirutil.ns import update_ns_module
+from mirutil.ns import rm_ns_module
 
-class GDUrl :
-    cur = 'https://github.com/imahdimir/g-d-TSETMC_ID-2-FirmTicker'
-    trg = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
-    mb2f = 'https://github.com/imahdimir/d-multi-BaseTickers-2-same-FirmTicker'
+update_ns_module()
+import ns
 
-gu = GDUrl()
-
-class ColName :
-    tid = 'TSETMC_ID'
-    ftic = 'FirmTicker'
-
-c = ColName()
+gdu = ns.GDU()
+c = ns.Col()
 
 def main() :
     pass
 
     ##
 
-    gd_trg = GithubData(gu.trg)
-    gd_trg.overwriting_clone()
-    ##
-    dft = gd_trg.read_data()
-    ##
-    dft = dft.astype(str)
-    ##
-    dft = dft.drop_duplicates()
-    assert dft[c.tid].is_unique
+    # Get the target data
+    gd_trg = GitHubDataRepo(gdu.trg)
+    gd_trg.clone_overwrite()
 
     ##
-    msk = dft[c.ftic].str.contains('پذيره')
-    df1 = dft[msk]
+    df_fp = gd_trg.data_fp
+    df = pd.read_excel(df_fp , dtype = str)
+
+    ##
+    df = df.drop_duplicates()
+    assert df[c.tse_id].is_unique
+
+    ##
+    msk = df[c.ftic].str.contains('پذيره')
+    df1 = df[msk]
     assert df1.empty
 
     ##
+    # Get the data of multi base tickers to the same firm ticker
+    gd_mb2f = GitHubDataRepo(gdu.mbt)
+    gd_mb2f.clone_overwrite()
 
-    gd_mb2f = GithubData(gu.mb2f)
-    gd_mb2f.overwriting_clone()
     ##
-    dfz = gd_mb2f.read_data()
-    ##
-    dfz = dfz.astype(str)
-    ##
-    dfz.set_index(c.tid , inplace = True)
-    ##
+    dfm_fp = gd_mb2f.data_fp
+    dfm = pd.read_csv(dfm_fp , dtype = str)
 
-    dft['ft'] = dft[c.tid].map(dfz[c.ftic])
     ##
-    msk = dft['ft'].notna()
-    df1 = dft[msk]
-    ##
-    dft.loc[msk , c.ftic] = dft['ft']
-    ##
-    dft = dft[[c.tid , c.ftic]]
-    ##
+    dfm = dfm.set_index(c.tse_id)
 
-    dft.sort_values(c.ftic , inplace = True)
     ##
-    dftp = gd_trg.data_fp
-    snxl(dft , dftp)
+    nc = 'ft'
+    df[nc] = df[c.tse_id].map(dfm[c.ftic])
+
+    ##
+    msk = df[nc].notna()
+    df1 = df[msk]
+
+    ##
+    df.loc[msk , c.ftic] = df[nc]
+
+    ##
+    df = df.drop(columns = nc)
+
+    ##
+    # cols order
+    df = df[[c.tse_id , c.ftic]]
+
+    ##
+    df = df.sort_values(c.ftic)
+
+    ##
+    save_df_as_a_nice_xl(df , df_fp)
+
     ##
     msg = 'governed by: '
-    msg += gu.cur
-    ##
+    msg += gdu.slf
+    print('commit msg is: ' , msg)
 
+    ##
     gd_trg.commit_and_push(msg)
 
     ##
-
     gd_trg.rmdir()
     gd_mb2f.rmdir()
 
     ##
+    rm_ns_module()
 
 ##
-if __name__ == '__main__' :
+
+
+if __name__ == "__main__" :
     main()
-
-##
+    print(f'{Path(__file__).name} Done!')
